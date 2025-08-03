e7e127c6d98d1ec37c4309964d4afcf3556bb8cb
Pavel Barabanov <barabanovpv95@gmail.com>
2025-08-03 12:42:31 +0300

    feat: blogrenderer: finish

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -1,10 +1,43 @@
 package blogrenderer
 
 import (
-	"io"
-	"html/template"
 	"embed"
+	"html/template"
+	"io"
+	"strings"
+
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/parser"
 )
+
+var (
+	//go:embed "templates/*"
+	postTemplates embed.FS
+)
+
+type PostRenderer struct {
+	templ    *template.Template
+	mdParser *parser.Parser
+}
+
+func NewPostRenderer() (*PostRenderer, error) {
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
+
+	extensions := parser.CommonExtensions | parser.AutoHeadingIDs
+	parser := parser.NewWithExtensions(extensions)
+	return &PostRenderer{templ: templ, mdParser: parser}, nil
+}
+
+func (r *PostRenderer) Render(w io.Writer, p Post) error {
+	return r.templ.ExecuteTemplate(w, "blog.gohtml", newPostVM(p, r))
+}
+
+func (r *PostRenderer) RenderIndex(w io.Writer, posts []Post) error {
+	return r.templ.ExecuteTemplate(w, "index.gohtml", posts)
+}
 
 type Post struct {
 	Title       string
@@ -13,20 +46,17 @@ type Post struct {
 	Tags        []string
 }
 
-var (
-	//go:embed "templates/*"
-	postTemplates embed.FS
-)
+func (p Post) SanitisedTitle() string {
+	return strings.ToLower(strings.ReplaceAll(p.Title, " ", "-"))
+}
 
-func Render(w io.Writer, post Post) error {
-	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
-	if err != nil {
-		return err
-	}
+type postViewModel struct {
+	Post
+	HTMLBody template.HTML
+}
 
-	if err := templ.Execute(w, post); err != nil {
-		return err
-	}
-
-	return nil
+func newPostVM(p Post, r *PostRenderer) postViewModel {
+	vm := postViewModel{Post: p}
+	vm.HTMLBody = template.HTML(markdown.ToHTML([]byte(p.Body), r.mdParser, nil))
+	return vm
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module example.com/hello
 
 go 1.24.5
 
-require github.com/approvals/go-approval-tests v1.6.0 // indirect
+require (
+	github.com/approvals/go-approval-tests v1.6.0 // indirect
+	github.com/gomarkdown/markdown v0.0.0-20250731182530-5d03d1963446 // indirect
+)
