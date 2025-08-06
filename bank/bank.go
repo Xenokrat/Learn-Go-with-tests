9131f6f40ed0bacb7fcada137e3c6b47294c1b44
Pavel Barabanov <barabanovpv95@gmail.com>
2025-08-06 07:59:07 +0300

    feat: bank

---
+++ bank/bank.go
@@ -0,0 +1,65 @@
+package main
+
+type Transaction struct {
+	From string
+	To   string
+	Sum  float64
+}
+
+func NewTransaction(from, to Account, sum float64) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: sum}
+}
+
+type Account struct {
+	Name    string
+	Balance float64
+}
+
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(
+		transactions,
+		applyTransaction,
+		account,
+	)
+}
+
+func BalanceFor(transactions []Transaction, name string) float64 {
+	adjustBalance := func(currentBalance float64, t Transaction) float64 {
+		if t.From == name {
+			return currentBalance - t.Sum
+		}
+		if t.To == name {
+			return currentBalance + t.Sum
+		}
+		return currentBalance
+	}
+
+	return Reduce(transactions, adjustBalance, 0.0)
+}
+
+func Reduce[A, B any](collection []A, f func(B, A) B, initialValue B) B {
+    var result = initialValue
+    for _, x := range collection {
+        result = f(result, x)
+    }
+    return result
+}
+
+func applyTransaction(a Account, transaction Transaction) Account {
+	if transaction.From == a.Name {
+		a.Balance -= transaction.Sum
+	}
+	if transaction.To == a.Name {
+		a.Balance += transaction.Sum
+	}
+	return a
+}
+
+func Find[A any](items []A, predicate func(A) bool) (value A, found bool) {
+	for _, v := range items {
+		if predicate(v) {
+			return v, true
+		}
+	}
+	return
+}
